0a22f59944de0051bc698b897f17f501c390b305
bill <manbil03@gmail.com>
2020-03-07 20:09:53 +1030

    init charting project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Container from '@material-ui/core/Container';
 import './App.css';
+import BarChart from './barchart';
+import PieChart from './piechart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <BarChart></BarChart>
+      <PieChart></PieChart>
+    </Container>
   );
 }
 
---
+++ src/piedata.js
@@ -0,0 +1,38 @@
+export default [
+    {
+      "id": "7b+",
+      "label": "7b+",
+      "value": 387,
+      "color": "hsl(163, 70%, 50%)"
+    },
+    {
+      "id": "7b",
+      "label": "7b",
+      "value": 149,
+      "color": "hsl(341, 70%, 50%)"
+    },
+    {
+      "id": "6b",
+      "label": "6b",
+      "value": 239,
+      "color": "hsl(71, 70%, 50%)"
+    },
+    {
+      "id": "6b+",
+      "label": "6b+",
+      "value": 218,
+      "color": "hsl(317, 70%, 50%)"
+    },
+    {
+      "id": "7a",
+      "label": "7a",
+      "value": 57,
+      "color": "hsl(83, 70%, 50%)"
+    },
+    {
+      "id": "6c",
+      "label": "7c",
+      "value": 389,
+      "color": "hsl(422, 60%, 30%)"
+    }
+  ]
